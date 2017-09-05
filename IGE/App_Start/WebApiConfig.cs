d97dd41056df08409bcb8762f01fdd5f51a935f8
pandi <pandi.bluewings@gmail.com>
2017-09-05 21:03:42 +0530

    Json format Work added formatters

--- IGE/App_Start/WebApiConfig.cs
+++ IGE/App_Start/WebApiConfig.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http.Headers;
 using System.Web.Http;
 
 namespace IGE
@@ -23,6 +24,11 @@ namespace IGE
             // To disable tracing in your application, please comment out or remove the following line of code
             // For more information, refer to: http://www.asp.net/web-api
             config.EnableSystemDiagnosticsTracing();
+
+            config.Formatters.Remove(config.Formatters.XmlFormatter);
+            config.Formatters.JsonFormatter.SupportedMediaTypes.Add(new MediaTypeHeaderValue("application/json"));
+
+
         }
     }
 }
